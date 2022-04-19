e392b9ea6085cb9d29e2d671f45d5907cbccf573
Mustafa <mbin828@gmail.com>
2022-04-19 16:59:30 -0400

    sign out done

--- src/screens/accInfo.jsx
+++ src/screens/accInfo.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "../styles/Login.css";
-import Button from "@mui/material/Button";
-import EditIcon from "@mui/icons-material/Edit";
-import KeyIcon from "@mui/icons-material/Key";
+import { Link, useNavigate } from "react-router-dom";
 import { store } from "../state/store";
-import { useRef } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  let navigate = useNavigate();
   return (
     <div className="acc-main-container">
-      <h1>Acc Info</h1>
+      <Link
+        to={"/"}
+        onClick={() => {
+          localStorage.clear();
+          store.dispatch({ type: "set", payload: { name: "Guest" } });
+          navigate("/");
+        }}
+      >
+        <h1>Sign Out</h1>
+      </Link>
     </div>
   );
 }
